963c2b4f2d49e70720d37b887a8726c0724c2037
cpucorecore <cpucorecore@gmail.com>
2019-09-28 22:48:48 +0800

    ch1: helloworld

---
+++ ch1/helloworld/helloworld.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var (
+	i = 10
+	s = "abc"
+)
+
+func main() {
+	fmt.Println("Hello, World")
+	fmt.Println(i, s, "\n")
+	i = 100
+	s = "edf"
+	fmt.Println(i, s, "\n")
+}
